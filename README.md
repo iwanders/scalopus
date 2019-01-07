f0c85b7ab880f79258699cf2f21a909b496aa860
Ivor Wanders <ivor@iwanders.net>
2019-01-06 21:20:48 -0500

    Remove serialization test add licenses.

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 Retry the cmakelists:
 ```bash
 # build seasocks:
-rm -rf * && cmake ../../3rd_party/seasocks/ -DEFLATE_SUPPORT=off && make 
+rm -rf * && cmake ../../3rd_party/seasocks/ && make 
 # build scalopus:
 rm -rf * && nlohmann_json_DIR=$(pwd)/../json/ Seasocks_DIR=$(pwd)/../seasocks/ cmake ../../repo/  && make -j8
+# run tests;
+make -j8 && make test
 ```
--- scalopus_lttng/test/test_endpoint_scope_tracing.cpp
+++ scalopus_lttng/test/test_endpoint_scope_tracing.cpp
@@ -1,14 +1,52 @@
+/*
+  Copyright (c) 2019, Ivor Wanders
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+  Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include <iostream>
 #include "scalopus_lttng/endpoint_scope_tracing.h"
+#include "scalopus_lttng/scope_tracing.h"
+#include "scalopus_transport/transport_mock.h"
 
 template <typename A, typename B>
-void test(const A& a, const B& b)
+void test_map(const A& a, const B& b)
 {
   bool equal = (a.size() == b.size()) &&
                std::equal(a.begin(), a.end(), b.begin(), [](auto& c, auto& d) { return c.first == d.first; });
   if (!equal)
   {
     ::exit(1);
+  }
+}
+
+template <typename A, typename B>
+void test(const A& a, const B& b)
+{
+  if (a != b)
+  {
+    std::cerr << "a (" << a << ") != b (" << b << ")" << std::endl;
+    exit(1);
   }
 }
 int main(int /* argc */, char** /* argv */)
@@ -18,19 +56,28 @@ int main(int /* argc */, char** /* argv */)
   test_mapping[1] = "bar";
   test_mapping[2] = "buz";
 
-  scalopus::EndpointScopeTracing::ProcessTraceMap process_mapping;
-  process_mapping[5] = test_mapping;
-  process_mapping[11] = test_mapping;
-  process_mapping[10] = { { 3, "foo" } };
-
-  auto serialized = scalopus::EndpointScopeTracing::serializeMapping(process_mapping);
-  auto deserialized = scalopus::EndpointScopeTracing::deserializeMapping(serialized);
-
-  test(process_mapping, deserialized);
-  for (auto& pid_map : test_mapping)
+  for (const auto& id_name : test_mapping)
   {
-    test(process_mapping[pid_map.first], deserialized[pid_map.first]);
+    scalopus::ScopeTraceTracker::getInstance().insert(id_name.first, id_name.second);
   }
+
+  // Create the endpoint.
+  auto server_endpoint = std::make_shared<scalopus::EndpointScopeTracing>();
+
+  // Create mock server and client.
+  auto server = scalopus::transportServerMock();
+  server->addEndpoint(server_endpoint);
+
+  auto client = scalopus::transportClientMock(server);
+  // Add the endpoint to the client.
+  auto client_endpoint = std::make_shared<scalopus::EndpointScopeTracing>();
+  client_endpoint->setTransport(client);
+
+  auto retrieved_mapping = client_endpoint->mapping();
+
+  // there should be one entry in it, our current process id, that map should be identical to test_mapping.
+  test(retrieved_mapping.size(), 1u);
+  test_map(retrieved_mapping.begin()->second, test_mapping);
 
   return 0;
 }
