7c83dcf236179a68651009f70e26a25d5a86b23e
Ivor Wanders <ivor@iwanders.net>
2019-01-12 16:25:03 -0500

    Add native tracepoints and collector.

---
+++ scalopus_lttng/src/tracepoint_collector_native.cpp
@@ -0,0 +1,56 @@
+/*
+  Copyright (c) 2019, Ivor Wanders
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+  Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#include "tracepoint_collector_native.h"
+
+#include <iostream>
+namespace scalopus
+{
+TracePointCollectorNative& TracePointCollectorNative::getInstance()
+{
+  static TracePointCollectorNative instance;
+  return instance;
+}
+
+tracepoint_collector_types::ScopeBufferPtr TracePointCollectorNative::getBuffer()
+{
+  auto tid = static_cast<unsigned long>(pthread_self());
+  std::cout << "getBuffer called by : " << tid<< std::endl;
+  if (exists(tid))
+  {
+    std::cout << "Returning already existing" << std::endl;
+    return getMap()[tid];
+  }
+  else
+  {
+    // make the map.
+    std::cout << "Making the map" << std::endl;
+    auto buffer = std::make_shared<tracepoint_collector_types::ScopeBuffer>(tracepoint_collector_types::EventContainer{10000});
+    insert(tid, buffer);
+    return buffer;
+  }
+  return nullptr;
+}
+}  // namespace scalopus
