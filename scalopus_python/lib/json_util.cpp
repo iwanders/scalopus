fb7a3ec40185ae2f995d65056c9fcd26b0bdfd7c
Ivor Wanders <ivor@iwanders.net>
2019-02-02 13:12:24 -0500

    Make pybind11::object <-> nlohmann::json bindings.

---
+++ scalopus_python/lib/json_util.cpp
@@ -0,0 +1,216 @@
+/*
+  Copyright (c) 2018-2019, Ivor Wanders
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+  * Neither the name of the author nor the names of contributors may be used to
+    endorse or promote products derived from this software without specific
+    prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#include "json_util.h"
+#include <set>
+namespace scalopus
+{
+using json = nlohmann::json;
+
+py::object jsonToPy(const json& data)
+{
+  // Superset checks
+  // is_primitive:  (string, number, boolean, or null).
+  // is_structured: (array or object)
+  // is_number: This includes both integer (signed and unsigned) and floating-point values.
+  // is_object: map-esque.
+  // is_array: vector/list-esque.
+  // is_number_integer: Unsigned or signed integer.
+  // is_discarded: Placed if the parser discarded an entry.
+
+  // Which leaves types:
+  // is_null: None
+  // is_boolean: bool
+  // is_number_unsigned: uint
+  // is_number_integer: int
+  // is_number_float: double
+  // is_string: string
+
+  if (data.is_null())
+  {
+    return py::none();
+  }
+
+  if (data.is_boolean())
+  {
+    return py::bool_(data.get<bool>());
+  }
+
+  if (data.is_number_unsigned())  // check unsigned first, this is subset of integer.
+  {
+    return py::int_(data.get<std::uint64_t>());
+  }
+
+  if (data.is_number_integer())
+  {
+    return py::int_(data.get<std::int64_t>());
+  }
+
+  if (data.is_number_float())
+  {
+    return py::float_(data.get<double>());
+  }
+
+  if (data.is_string())
+  {
+    return py::str(data.get<std::string>());
+  }
+
+  // Handle lists.
+  if (data.is_array())
+  {
+    py::list res;
+    for (const auto& v : data)
+    {
+      res.append(jsonToPy(v));
+    }
+    return res;
+  }
+
+  // Handle dictionaries.
+  if (data.is_object())
+  {
+    py::dict res;
+    for (auto it : data.items())
+    {
+      res[jsonToPy(it.key())] = jsonToPy(it.value());
+    }
+    return res;
+  }
+
+  if (data.is_discarded())
+  {
+    return py::object();  // return an empty object if the parser discarded something
+  }
+  // This can only happen if the json library adds a type.
+  throw py::value_error("Cannot convert provided json element to python.");
+  return py::none();
+}
+
+json pyToJson(const py::object& data)
+{
+  // Handle types in order from the pybind11 reference. State which implementations are missing.
+
+  // missing iterable
+
+  if (py::isinstance<py::str>(data))
+  {
+    return json{data.cast<std::string>()};
+  }
+
+  if (py::isinstance<py::bytes>(data))
+  {
+    return json{data.cast<std::vector<std::uint8_t>>()};
+  }
+
+  if (py::isinstance<py::none>(data))
+  {
+    return json{};
+  }
+
+  if (py::isinstance<py::bool_>(data))
+  {
+    return json{data.cast<bool>()};
+  }
+
+  if (py::isinstance<py::int_>(data))
+  {
+    return json{data.cast<std::int64_t>()};
+  }
+
+  if (py::isinstance<py::float_>(data))
+  {
+    return json{data.cast<double>()};
+  }
+  // missing weakref
+  // missing slice
+  // missing capsule
+
+  // tuple is handled like a sequence.
+  if (py::isinstance<py::sequence>(data))
+  {
+    std::vector<json> json_seq;
+    for (const auto& v : data.cast<py::sequence>())
+    {
+      json_seq.push_back(pyToJson(v.cast<py::object>()));
+    }
+    return json{json_seq};
+  }
+
+  if (py::isinstance<py::dict>(data))
+  {
+    py::dict our_dict = data.cast<py::dict>();
+    // use an intermediate <json, json> map, because assigning like json[json] = json is ambigious overload.
+    std::map<json, json> json_json_map;
+    json our_json_map;
+    for (const auto& k_v : our_dict)
+    {
+      json_json_map[pyToJson(k_v.first.cast<py::object>())] = pyToJson(k_v.second.cast<py::object>());
+    }
+    our_json_map = json_json_map;
+    return our_json_map;
+  }
+
+  // missing list: sequence should handle it.
+  // missing args
+  // missing kwargs
+  
+  if (py::isinstance<py::set>(data))
+  {
+    std::set<json> json_set;
+    for (const auto& v : data.cast<py::set>())
+    {
+      json_set.insert(pyToJson(v.cast<py::object>()));
+    }
+    return json{json_set};
+  }
+  // missing function
+  // missing buffer
+  // missing memoryview
+
+  // For missing entries, return json.
+  return json{};
+}
+
+}  // namespace scalopus
+
+namespace pybind11
+{
+
+void from_json(const nlohmann::json& data, object& python_value)
+{
+  python_value = scalopus::jsonToPy(data);
+}
+void to_json(const object& data, nlohmann::json& json_value)
+{
+  json_value = scalopus::pyToJson(data);
+}
+
+//  void to_json(const object& data, nlohmann::json& json_value);
+}  // namespace pybind11
--- scalopus_python/lib/json_util.h
+++ scalopus_python/lib/json_util.h
@@ -29,53 +29,60 @@
 */
 #pragma once
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#include <scalopus_interface/endpoint_manager.h>
-#include "json_util.h"
+#include <nlohmann/json.hpp>
+#include <iostream>
 
 namespace scalopus
 {
 namespace py = pybind11;
+using json = nlohmann::json;
 /**
- * @brief Custom function to data conversion. Since there is so many 'valid' ways in Python 2 and 3 to express bytes.
- * Strings, bytearray, buffer, list of integers.
- * @param obj The python object to convert.
- * @param outgoing the Data to assign into
- * @return True if data could be converted, false otherwise.
+ * @brief Convert a json variable into a python object.
  */
-bool pyToData(const py::object& obj, Data& outgoing);
+py::object jsonToPy(const nlohmann::json& data);
 
 /**
- * @brief Same as pyToData, but throws py::value_error if it fails.
+ * @brief Convert a python object into a json variable.
  */
-Data pyToData(const py::object& obj);
-
-/**
- * @brief Convert the data into the 'best' Python bytes representation; py:bytes
- */
-py::object dataToPy(const Data& data);
-
-class PyEndpoint : public Endpoint
-{
-public:
-  using Ptr = std::shared_ptr<PyEndpoint>;
-  using Endpoint::Endpoint;  // Inherit constructors
-
-  std::string getName() const override;
-  bool handle(Transport& transport, const Data& incoming, Data& outgoing) override;
-  bool unsolicited(Transport& transport, const Data& incoming, Data& outgoing) override;
-  void setTransport(Transport* transport) override;
-};
-
-class PendingResponse
-{
-public:
-  using Ptr = std::shared_ptr<PendingResponse>;
-  PendingResponse(Transport::PendingResponse resp);
-  py::object wait_for(double seconds);
-
-  Transport::PendingResponse resp_;
-};
-
-void add_scalopus_interface(py::module&);
+json pyToJson(const py::object& data);
 }  // namespace scalopus
+
+namespace pybind11
+{
+void from_json(const nlohmann::json& data, object& python_value);
+void to_json(const object& data, nlohmann::json& json_value);
+
+// Bindings for the pybind11.
+namespace detail
+{
+template <>
+struct type_caster<nlohmann::json>
+{
+public:
+  PYBIND11_TYPE_CASTER(nlohmann::json, _("json"));
+
+  /**
+   * Conversion part 1 (Python->C++):
+   */
+  bool load(handle src, bool)
+  {
+    // Cast it to an object, which works for data containers.
+    if (!isinstance<object>(src))
+        return false;
+    object s = reinterpret_borrow<object>(src);
+    // Then, value is a member attribute from the PYBIND11_TYPE_CASTER, we write into that the ooutput of pyToJson
+    value = scalopus::pyToJson(s);
+    return true;
+  }
+
+  /**
+   * Conversion part 2 (C++ -> Python)
+   */
+  static handle cast(nlohmann::json src, return_value_policy /* policy */, handle /* parent */)
+  {
+    object thing = scalopus::jsonToPy(src);
+    return thing.release();
+  }
+};
+}  // namespace detail
+}  // namespace pybind11
