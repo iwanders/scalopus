a0c9b78e27127fdb0e6e4e56b577b350604c64c3
Ivor Wanders <ivor@iwanders.net>
2019-02-03 11:56:43 -0500

    Import the correct shared object.

--- scalopus_python/scalopus/scalopus_python_lib.py
+++ scalopus_python/scalopus/scalopus_python_lib.py
@@ -1,7 +1,14 @@
 def __bootstrap__():
-   global __bootstrap__, __loader__, __file__
-   import sys, pkg_resources, imp
-   __file__ = pkg_resources.resource_filename(__name__,'../scalopus_python_lib.cpython-36m-x86_64-linux-gnu.so')
-   __loader__ = None; del __bootstrap__, __loader__
-   imp.load_dynamic(__name__,__file__)
+    global __bootstrap__, __loader__, __file__
+    import sys, pkg_resources, imp, sysconfig
+    # Use the ABI suffix, this is "" for python2, for python3 this call returns something like:
+    # ['cpython-36m-x86_64-linux-gnu']
+    so_abi_suffix = sysconfig.get_config_vars("SOABI")
+    so_suffix = ""
+    if so_abi_suffix:
+        if len(so_abi_suffix):
+            so_suffix = so_abi_suffix[0] + "." if so_abi_suffix[0] is not None else ""
+    __file__ = pkg_resources.resource_filename(__name__,'../scalopus_python_lib.{}so'.format(so_suffix))
+    __loader__ = None; del __bootstrap__, __loader__
+    imp.load_dynamic(__name__,__file__)
 __bootstrap__()
