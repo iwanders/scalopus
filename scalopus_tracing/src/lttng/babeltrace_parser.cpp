434c9f985c65d2f49fd2b940b15a94bf014d74b8
Ivor Wanders <ivor@iwanders.net>
2025-04-16 14:08:26 -0400

    Bump pybind11.

--- scalopus_tracing/src/lttng/babeltrace_parser.cpp
+++ scalopus_tracing/src/lttng/babeltrace_parser.cpp
@@ -28,6 +28,7 @@
   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #include "scalopus_tracing/babeltrace_parser.h"
+#include <array>
 #include <iostream>
 #include <sstream>
 
